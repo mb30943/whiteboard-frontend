593326e94c866365d19c8fdbe22c10cb5b340b01
mb30942 <mb30942@seeu.edu.mk>
2025-05-28 18:06:19 +0200

    WhiteBoard: Show real time users who joined

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -91,7 +91,10 @@ onMounted(() => {
   socket.on('draw', (data) => {
     drawFromServer(data);
   });
-
+socket.on('room-users', (users) => {
+  console.log('Users in the room:', users);
+  // Update UI accordingly
+});
   
 });
 
